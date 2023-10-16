7f97fa4140b17c4b3d7f4091a6a81c7c58ae02a7
bhanujggandhi <gandhibhanuj@gmail.com>
2023-10-16 12:48:33 +0530

    friend function updated

--- friendfunc.cpp
+++ friendfunc.cpp
@@ -6,19 +6,19 @@ using namespace std;
 A friend function in C++ is a function that is not a member of a class but is allowed to access the private and protected members of that class. It is declared inside the class definition using the friend keyword. Friend functions provide flexibility in terms of granting specific external functions access to class internals without necessarily making those members public.
 */
 
-class Base {
+class Complex {
    private:
-    int data;
-    int d2;
+    int a;
+    int b;
 
    public:
-    // Base() {
-    //     data = 100;
-    // }
+    Complex(int a, int b) : a(a), b(b){};
 
-    Base(int d1) : data(d1), d2(d1 * d1 + 7){};
+    void showData() {
+        cout << "Complex Number: " << a << " + " << b << " i" << endl;
+    }
 
-    friend int func(const Base &obj);
+    friend int add(Complex &obj);
 };
 
 /*
@@ -26,13 +26,14 @@ The friend function definition is outside the class, but it is able to access th
 of Base objects because it's declared as a friend.
 */
 
-int func(const Base &obj) {
-    return obj.d2;
+// Me as a programmer can have my own implementation of add function
+int add(Complex &obj) {
+    return obj.a + obj.b;
 }
 
 int main() {
-    Base b(10);
+    Complex c1(10, 12);
 
-    cout << func(b) << endl;
+    cout << add(c1) << endl;
     return 0;
 }
